1f134a950810f870530bf3b5ab17459118481109
Vu Le <thenewvu@gmail.com>
2017-08-19 01:30:50 +0700

    Fix hardcoding event name

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ import { Children, Component, cloneElement } from 'react'
 
 export default class EventData extends Component {
   render = () => cloneElement(Children.only(this.props.children), this.enhancedProp)
-  onEvent = (...args) => Children.only(this.props.children).props.onPress(this.props.data, ...args)
+  onEvent = (...args) => Children.only(this.props.children).props[this.props.event](this.props.data, ...args)
   enhancedProp = { [this.props.event]: this.onEvent }
 }
